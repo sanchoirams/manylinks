cc3af5cd3b254c8822697218d5fd75d9627e5b76
Francisco André <fandrefh@gmail.com>
2023-06-22 21:17:14 -0300

    Adicionado static root

--- manylinks/settings.py
+++ manylinks/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
